34af80aeed376acd75363d22295030f891fffa35
Karthik.Nagabandi <Karthik.Nagabandi@NAOU10934>
2016-09-24 16:51:06 -0500

    Add project files.

---
+++ src/PersonalWebpage/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿//site.js
+// self executing anonymous function
+// nameless function that is execute immediately
+
+(function () {
+    var ele = $("#username");
+    ele.text("Karthik Kumar Nagabandi");
+
+    var main = $("#Main");
+    main.on("mouseenter", function () {
+        main.style = "background-color: #888;";
+    });
+
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    var $sidebarAndWrapper = $("#Sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        }
+        else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
+
