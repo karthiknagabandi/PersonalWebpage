f8da991d43aeea8901a6536091c17c93a63b094f
Karthik.Nagabandi <Karthik.Nagabandi@NAOU10934>
2016-09-24 20:19:13 -0500

    SendMessage in ContactPage

--- src/PersonalWebpage/Startup.cs
+++ src/PersonalWebpage/Startup.cs
@@ -7,16 +7,54 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PersonalWebpage.Service;
+using Microsoft.Extensions.Configuration;
 
 namespace PersonalWebpage
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             // Dependency Injection
+            //Registering own services
+            // AddTransient - Transient creates an instance of debugmail service when needed, and keeps is cached around              
+            //services.AddTransient<IMailService, DebugMailService>();
+
+            //AddScoped - create an instance of debugmail for each set of requests and reused during the requests
+            //AddSingleton  - create on instance the first time we needed and pass that instance over and over again
+
+            //
+            services.AddSingleton(_config);
+
+            if (_env.IsEnvironment("Development") || _env.IsEnvironment("Testing"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                // Implement the service
+            }
+
+
             services.AddMvc();
         }
 
